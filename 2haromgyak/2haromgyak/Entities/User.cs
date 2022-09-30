2c9d27c6ff9344896ac38b2d0aa27707aa94f2e0
ulj3eb <ulj3eb@SZA0025774>
2022-09-30 10:50:05 +0200

    first update

---
+++ 2haromgyak/2haromgyak/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿Using System;
+
+namespace 2haromgyak.Entities
+{
+	public class User
+	{
+		public User()
+		{
+			public Guid ID { get; set; } = Guid.NewGuid();
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public string FullName
+			{
+				get
+				{
+				return FirstName + " " + LastName;
+				}
+			}
+	}
+	}
+}
