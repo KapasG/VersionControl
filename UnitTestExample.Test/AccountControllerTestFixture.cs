221b69ec71ce936bec681664b433ff1d48e634e5
ulj3eb <ulj3eb@SZA0025774>
2022-11-25 10:46:08 +0100

    regex

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,12 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
-        [Test]
-        public void TestValidateEmail(string email, bool expectedResult);
+        [Test,
+         TestCase("abcd1234", false),
+         TestCase("irf@uni-corvinus", false),
+         TestCase("irf.uni-corvinus.hu", false),
+         TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+         TestCase("asfdasfd", false),
+         TestCase("SADF1234", false),
+         TestCase("asdf1234", false),
+         TestCase("ASdf1234", true)
+        ]
+        public void TestValidatePassword(string pass, bool expectedResult)
+        {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidatePassword(pass);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
